619ca70853f95269054c25e4996eb3a484f6d72c
radthenone <krzyzak.codes@gmail.com>
2024-03-11 17:10:17 +0100

    task remove

--- backend/src/common/tasks.py
+++ backend/src/common/tasks.py
@@ -1,30 +1,6 @@
-from datetime import datetime, timedelta
-
-from celery import shared_task
-from ninja.errors import HttpError
-
 from src.core.celery import celery
 
-time_start = datetime.now() + timedelta(seconds=60)
-time_end = time_start + timedelta(minutes=3)
 
-celery.conf.beat_schedule = {
-    "divide": {
-        "task": "src.common.tasks.divide",
-        "schedule": time_start,
-        "options": {"expires": time_end},
-    },
-}
-
-
-@shared_task
+@celery.task
 def divide(x, y):
     return str(x / y)
-
-
-def make_divide(x, y):
-    try:
-        result = divide.delay(x, y)
-        return result
-    except Exception:
-        return None
