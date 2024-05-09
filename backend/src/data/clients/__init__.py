03ebf4b2c7919495e64f49df6575dd7a6d443b68
radthenone <krzyzak.codes@gmail.com>
2024-05-09 15:47:27 +0200

    add init email manager
    Signed-off-by: radthenone <krzyzak.codes@gmail.com>

--- backend/src/data/clients/__init__.py
+++ backend/src/data/clients/__init__.py
@@ -1,7 +1,9 @@
 from src.data.clients.celery_client import CeleryClient
+from src.data.clients.mail_client import MailClient
 from src.data.clients.redis_client import RedisClient
 
 __all__ = [
+    "MailClient",
     "RedisClient",
     "CeleryClient",
 ]
